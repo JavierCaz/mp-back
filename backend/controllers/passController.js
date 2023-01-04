4dc67bb15227ac9026b0b71b6e7c8804c7532419
Javier Cazares <javiercaz98@hotmail.com>
2023-01-03 23:38:04 -0700

    mongo connection

--- backend/controllers/passController.js
+++ backend/controllers/passController.js
@@ -1,18 +1,20 @@
-const getPasses = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getPasses = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get pass'})
-}
+})
 
-const setPass = (req, res) => {
+const setPass = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Post pass'})
-}
+})
 
-const updatePass = (req, res) => {
+const updatePass = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update pass ${req.params.id}`})
-}
+})
 
-const deletePass = (req, res) => {
+const deletePass = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete pass ${req.params.id}`})
-}
+})
 
 module.exports = {
     getPasses,
