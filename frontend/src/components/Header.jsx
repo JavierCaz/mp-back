f8892f5f69e7db94aff51fe5f8b1c55c78767d83
Javier Cazares <javiercaz98@hotmail.com>
2023-01-08 12:55:44 -0700

    register and login implemented

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,22 @@
+import { useSelector, useDispatch } from "react-redux"
+import { logout, reset } from "features/auth/authSlice"
+import { useNavigate } from "react-router-dom"
+
 const Header = ({pages = []}) => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
+    const onLogout = () => {
+        dispatch(logout())
+        dispatch(reset())
+        navigate('/')
+    }
+
     return (
         <header>
             <nav style={{display:'grid', gridAutoFlow:'column', gap:'1em', justifyContent:'start', padding:'0.5em'}}>
-                {pages}
+                {user ? <button onClick={onLogout}>Logout</button> : pages}
             </nav>
         </header>
     )
